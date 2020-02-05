19e399f18173decd6400ab3789ea0531bcc64966
professornpc@gmail.com <TakTak123!>
2020-02-04 21:12:08 -0300

    Teste Funcional(Instrumental) para Main Activity Adição de primeira classe de teste funcional. Tenta validar se os campos de entrada estão presentes.

---
+++ app/src/androidTest/java/com/belval/aoug/MainActivityInstrumentedTest.java
@@ -0,0 +1,53 @@
+package com.belval.aoug;
+
+import android.content.Context;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class MainActivityInstrumentedTest {
+    @Test
+    public void possuiCampoEtanol() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+        String packageName = appContext.getPackageName();
+
+        int id = getResourceId("editAlcool");
+        onView(withId(id)).perform(click());;
+
+        //assertEquals("com.belval.aoug", appContext.getPackageName());
+    }
+
+    public void possuiCampoGasolina() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+        String packageName = appContext.getPackageName();
+
+        int id = getResourceId("editGasolina");
+        onView(withId(id)).perform(click());
+
+        //assertEquals("com.belval.aoug", appContext.getPackageName());
+    }
+
+    private static int getResourceId(String id) {
+        Context targetContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        String packageName = targetContext.getPackageName();
+        return targetContext.getResources().getIdentifier(id, "id", packageName);
+    }
+}
